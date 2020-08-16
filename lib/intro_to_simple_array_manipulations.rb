3389a09820f48dad89a61ad38eab1a0ff36228f2
Tej Charfi <charfi@usc.edu>
2020-08-16 17:13:23 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_concat(arr1, arr2)
+  arr1.concat(arr2)
+end
+
+def using_insert(arr, element)
+  arr.insert(4,element)
+end
+
+def using_uniq(arr)
+  arr.uniq!
+end
+
+def using_flatten(arr)
+  arr.flatten
+end
+  
+def using_delete(arr, str)
+  arr.delete(str)
+end
+
+def using_delete_at(arr, num)
+  arr.delete_at(num)
+end
